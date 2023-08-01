f5f02cf5b852ac4aa5cb51b1f75b7855028cf340
User <user@Users-MacBook-Pro.local>
2023-08-01 21:31:09 +0300

    ref4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-    const dateInputRef = useRef(null);
-
-    const focus = () => {
-        console.log("yes");
-        dateInputRef.current.click();
-    };
-    const [dateParagraf, setDateParagraf] = useState("");
+    const [dateParagraf, setDateParagraf] = useState("Виберіть День");
 
     let [seconds, setSeconds] = useState(0);
     let [precentOfseconds, setPrecentOfSeconds] = useState(0);
@@ -25,54 +20,72 @@ function App() {
     let [days, setDays] = useState(0);
     let [precentOfDays, setPrecentOfDays] = useState(0);
     const precentDaysRef = useRef(null);
-    let total = "";
-    let stopTime;
+    let [total, setTotal] = useState("");
+    let [stopTime, setStopTime] = useState(false);
 
     const getTime = (e) => {
+        if (stopTime) {
+            clearInterval(stopTime);
+            setStopTime(null);
+            setTotal(false);
+        }
+
         if (Date.parse(e.target.value) > Date.parse(new Date())) {
             setDateParagraf(e.target.value);
+            console.log("e.target", e.target.value);
+            let stop = setInterval(() => totalTime(e.target.value), 1000);
+            setStopTime(stop);
         } else {
-            setDateParagraf("No coorect");
             clearInterval(stopTime);
-            return;
+            setDateParagraf("Некоректна дата!");
+            setStopTime(null);
+            setTotal(0);
         }
-
-        function totalTime() {
-            total = Date.parse(e.target.value) - Date.parse(new Date());
-
-            setSeconds((seconds = Math.floor((total / 1000) % 60)));
-            setPrecentOfSeconds((precentOfseconds = 100 - (100 / 60) * seconds));
-            precentSecRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfseconds}%, #303238 ${precentOfseconds}% 100%)`;
-
-            setMinutes((minutes = Math.floor((total / 1000 / 60) % 60)));
-            setPrecentOfMinutes((precentOfMinutes = 100 - (100 / 60) * minutes));
-            precentMinRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfMinutes}%, #303238 ${precentOfMinutes}% 100%)`;
-
-            setHours((hours = Math.floor((total / (1000 * 60 * 60)) % 24)));
-            setPrecentOfHours((precentOfHours = 100 - (100 / 24) * hours));
-            precentHoursRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfHours}%, #303238 ${precentOfHours}% 100%)`;
-
-            setDays((days = Math.floor(total / (1000 * 60 * 60 * 24))));
-            setPrecentOfDays((precentOfDays = 100 / days));
-            precentDaysRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfDays}%, #303238 ${precentOfDays}% 100%)`;
-            if (days === 0) {
-                setPrecentOfDays(0);
-            }
-            if (total === 0) {
-                clearInterval(stopTime);
-            }
-        }
-        totalTime();
-        stopTime = setInterval(totalTime, 1000);
     };
+    function totalTime(e) {
+        const newTotal = Date.parse(e) - Date.parse(new Date());
+        setTotal((total = newTotal));
+
+        setSeconds((seconds = Math.floor((total / 1000) % 60)));
+        setPrecentOfSeconds((precentOfseconds = 100 - (100 / 60) * seconds));
+        if (precentSecRef.current) {
+            precentSecRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfseconds}%, #303238 ${precentOfseconds}% 100%)`;
+        }
+
+        setHours((hours = Math.floor(((total / (1000 * 60 * 60)) % 24) - 3)));
+        setPrecentOfHours((precentOfHours = 100 - (100 / 24) * hours));
+        if (precentHoursRef.current) {
+            precentHoursRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfHours}%, #303238 ${precentOfHours}% 100%)`;
+        }
+
+        setMinutes((minutes = Math.floor((total / 1000 / 60) % 60)));
+        setPrecentOfMinutes((precentOfMinutes = 100 - (100 / 60) * minutes));
+        if (precentMinRef.current) {
+            precentMinRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfMinutes}%, #303238 ${precentOfMinutes}% 100%)`;
+        }
+
+        setDays((days = Math.floor(total / (1000 * 60 * 60 * 24))));
+        setPrecentOfDays((precentOfDays = 100 / days));
+        if (precentDaysRef.current) {
+            precentDaysRef.current.style = `background: conic-gradient(#02c2c2 0 ${precentOfDays}%, #303238 ${precentOfDays}% 100%)`;
+        }
+
+        if (days === 0) {
+            setPrecentOfDays(0);
+        }
+        if (total === 0) {
+            clearInterval(stopTime);
+            setStopTime(null);
+        }
+    }
 
     return (
         <div className='wrapper'>
             <section className='section'>
                 <div className='input-date-block'>
                     <div>
-                        <span>Виьбрати дату</span>
-                        <input ref={dateInputRef} onClick={focus} type='date' name='date' id='input-date' onInput={getTime} />
+                        <input type='date' name='date' id='input-date' onChange={getTime} />
+                        {/* <span>Виьбрати дату</span> */}
                     </div>
                     <p>{dateParagraf}</p>
                 </div>
@@ -80,7 +93,7 @@ function App() {
                 <div ref={precentDaysRef} className='time-block' id='days'>
                     <div>
                         <ul>
-                            <li>{days}</li>
+                            <li>{days < 10 ? `0${days}` : days}</li>
                             <li>Days</li>
                         </ul>
                     </div>
@@ -88,7 +101,7 @@ function App() {
                 <div ref={precentHoursRef} className='time-block' id='hours'>
                     <div>
                         <ul>
-                            <li>{hours}</li>
+                            <li>{hours < 10 ? `0${hours}` : hours}</li>
                             <li>Hours</li>
                         </ul>
                     </div>
@@ -96,7 +109,7 @@ function App() {
                 <div ref={precentMinRef} className='time-block' id='minutes'>
                     <div>
                         <ul>
-                            <li>{minutes}</li>
+                            <li>{minutes < 10 ? `0${minutes}` : minutes}</li>
                             <li>Minutes</li>
                         </ul>
                     </div>
@@ -104,7 +117,7 @@ function App() {
                 <div ref={precentSecRef} className='time-block' id='seconds'>
                     <div>
                         <ul>
-                            <li>{seconds}</li>
+                            <li>{seconds < 10 ? `0${seconds}` : seconds}</li>
                             <li>Seconds</li>
                         </ul>
                     </div>
